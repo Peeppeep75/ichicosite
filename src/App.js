5bd0b351ef72c2a6bb90c335f5dd3254340a5ee4
Peeppeep75 <n_n_bsk.75@icloud.com>
2024-01-23 15:54:50 +0900

    コンソールの変更

--- src/App.js
+++ src/App.js
@@ -2,22 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 
 import React, { useState } from 'react';
+
 function App() {
   const [count, setCount] = useState(0);
   const [isSadMode, setIsSadMode] = useState(false);
 
   const smileemojis = ["😀", "😃", "😄", "😁", "😆", "😅", "😂", "🤣", "😊", "😇"];
-  const sademojis = ["😞", "😒", "🙄", "😭", "😵‍💫", "😮‍💨", "😨", "😫" , "😣", "🫥"];
-  const arrayLength = isSadMode ? sademojis.length : smileemojis.length; 
+  const sademojis = ["😞", "😒", "🙄", "😭", "😵‍💫", "😮‍💨", "😨", "😫", "😣", "🫥"];
+  const arrayLength = isSadMode ? sademojis.length : smileemojis.length;
   const arrayIndex = count % arrayLength;
-  const currentEmoji = isSadMode ? sademojis[arrayIndex] : smileemojis[arrayIndex]; 
+  const currentEmoji = isSadMode ? sademojis[arrayIndex] : smileemojis[arrayIndex];
+  const niconicoemojis = smileemojis[3]
+  const shikushikuemojis = sademojis[3]
+
 
   const handleDontPush = () => {
+    console.log("shikushiku");
+    console.log("sademojis:", shikushikuemojis);
     setIsSadMode(true);
     setCount(count - 1);
   };
 
   const handleMakeSmile = () => {
+    console.log("niconico");
+    console.log("smileemojis:", niconicoemojis);
     setIsSadMode(false);
     setCount(count + 1);
   };
