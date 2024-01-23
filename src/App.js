97e5902b08f55eaf807f6c6a62facaafeb908bf5
Peeppeep75 <n_n_bsk.75@icloud.com>
2024-01-23 14:45:48 +0900

    ボタンの追加

--- src/App.js
+++ src/App.js
@@ -4,21 +4,34 @@ import './App.css';
 import React, { useState } from 'react';
 function App() {
   const [count, setCount] = useState(0);
-  const emojis = ["😀", "😃", "😄", "😁", "😆", "😅", "😂", "🤣", "😊", "😇"];
-  const currentEmoji = emojis[count % emojis.length];
+  const smileemojis = ["😀", "😃", "😄", "😁", "😆", "😅", "😂", "🤣", "😊", "😇"];
+  const sademojis =["😞", "😒", "🙄", "😭", "😵‍💫", "😮‍💨", "😨", "😫" , "😣", "🫥"];
+  const arrayLength = smileemojis.length
+  const arrayIndex = count % arrayLength
+  const emojiNicoNico = smileemojis[3]
+  console.log("emoji", smileemojis)
+  console.log("niconico", emojiNicoNico)
+  const currentEmoji = smileemojis[arrayIndex];
+
+
   return (
     <div className="App">
   <header className="App-header">
     <div className="emoji">{currentEmoji}</div>
     <p>You clicked {count} times</p>
     <p>count % emojis.length: {count % emojis.length}</p>
+    <button onClick={() => setCount(count + 1)}>Don't push</button>
     <button onClick={() => setCount(count + 1)}>
-      Click me
-    </button>
+
+      <></>
+      Make smile  </button>
+    
+
   </header>
 </div>
+);
 
-  );
+
 }
 
 export default App;
